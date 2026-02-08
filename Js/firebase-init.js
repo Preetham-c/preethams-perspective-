50e60c58e71cf8d02d27227acf67acb2fd23e806
Preetham-c <Preethamchannup@gmail.com>
2026-02-08 20:31:20 +0530

    Update firebase-init.js

--- Js/firebase-init.js
+++ Js/firebase-init.js
@@ -6,18 +6,16 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-
 
 // Your Firebase configuration
 export const firebaseConfig = {
-    apiKey: "AIzaSyD8D010tVKFdISBG9ceXwBDng0JtJksnRYY",
-    authDomain: "preetham-perspective.firebaseapp.com",
-    projectId: "preetham-perspective",
-    storageBucket: "preetham-perspective.firebasestorage.app",
-    messagingSenderId: "392987040131",
-    appId: "1:392987040131:web:ee46f4ffc14ff72a33eb6e"
+  apiKey: "AIzaSyD8D010tVKFdISBG9ceXwBDng0JtJksnRYY",
+  authDomain: "preetham-perspective.firebaseapp.com",
+  projectId: "preetham-perspective",
+  storageBucket: "preetham-perspective.firebasestorage.app",
+  messagingSenderId: "392987040131",
+  appId: "1:392987040131:web:ee46f4ffc14ff72a33eb6e"
 };
 
-// Initialize Firebase App
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-
-// Export Firebase services for use everywhere
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
